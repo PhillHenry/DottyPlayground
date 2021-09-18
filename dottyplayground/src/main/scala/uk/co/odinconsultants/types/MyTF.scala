d11bd7eb9ee03a97d34dad7321de4c48a27304c0
Phillip Henry <PhillHenry@gmail.com>
2021-09-18 10:02:25 +0100

    Playing with kjaer compiletime.

--- dottyplayground/src/main/scala/uk/co/odinconsultants/types/MyTF.scala
+++ dottyplayground/src/main/scala/uk/co/odinconsultants/types/MyTF.scala
@@ -1,14 +1,17 @@
 package uk.co.odinconsultants.types
 
 import io.kjaer.compiletime._
-//import io.kjaer.tensorflow.core._ // not sure where to get this from
 
 object MyTF {
-  type Dim = 2 #: 2 #: SNil
-  val dimensions = 2 #: 2 #: SNil
-//  val matrix = tf.zeros(2 #: 2 #: SNil)
+  type TwoXTwo = 2 #: 2 #: SNil
+  val twoXtwo = 2 #: 2 #: SNil
+  val twoXthree = 2 #: 3 #: SNil
+
+  def twoByTwo[F[_]](x: F[TwoXTwo]): Unit = println("2 x 2")
 
   def main(args: Array[String]): Unit = {
-    println(dimensions.getClass.getName)
+    println(twoXtwo.getClass.getName)
+    twoByTwo(List(twoXtwo))
+//    twoByTwo(List(twoXthree)) // doesn't compile - as expected
   }
 }
