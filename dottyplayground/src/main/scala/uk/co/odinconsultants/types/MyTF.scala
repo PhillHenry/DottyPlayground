2e5c65cf6254328a72fb9362f73b7fe95980f55d
Phillip Henry <PhillHenry@gmail.com>
2021-09-18 10:08:54 +0100

    TODO: need to make the other dimension a wild card.

--- dottyplayground/src/main/scala/uk/co/odinconsultants/types/MyTF.scala
+++ dottyplayground/src/main/scala/uk/co/odinconsultants/types/MyTF.scala
@@ -6,12 +6,17 @@ object MyTF {
   type TwoXTwo = 2 #: 2 #: SNil
   val twoXtwo = 2 #: 2 #: SNil
   val twoXthree = 2 #: 3 #: SNil
+  val threeXthree = 3 #: 3 #: SNil
 
   def twoByTwo[F[_]](x: F[TwoXTwo]): Unit = println("2 x 2")
+
+  def multiply[T <: Int & Singleton](x: 2 #: T #: SNil, y: T #: 3 #: SNil): Unit = println(s"Multiply $x by $y")
 
   def main(args: Array[String]): Unit = {
     println(twoXtwo.getClass.getName)
     twoByTwo(List(twoXtwo))
 //    twoByTwo(List(twoXthree)) // doesn't compile - as expected
+    multiply(twoXtwo, twoXthree)
+//    multiply(threeXthree, twoXthree) // doesn't compile - as expected
   }
 }
