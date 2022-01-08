b5cd26707d04ef93dc90b246c8243397f2c5eae2
Phillip Henry <PhillHenry@gmail.com>
2022-01-08 08:51:32 +0000

    Need to prove dynamic values are constrained.

--- dottyplayground/src/main/scala/uk/co/odinconsultants/types/singleton/MySingletons.scala
+++ dottyplayground/src/main/scala/uk/co/odinconsultants/types/singleton/MySingletons.scala
@@ -1,7 +1,7 @@
 package uk.co.odinconsultants.types.singleton
 
 import uk.co.odinconsultants.types.singleton.MySingletons.MyInt
-
+import scala.util.Try
 
 object MySingletons {
 
@@ -15,12 +15,23 @@ object MySingletons {
 
   def testSameInts(x: MyInt, y: MyInt): Unit = println(s"$x == $y")
 
+  type MyBinary = 1 | 0
+
+  def accept(binary: MyBinary): Unit = println(s"$binary is binary")
+
   def main(args: Array[String]): Unit = {
     testPhrase("old fashioned way")
     testPhrase("old fashioned way 2")
 //    test("old fashioned way 3") // Fantastic! Does not compile
     testSameInts(1, 2)
     testHKTs(List(1), List(2))
+    accept(1)
+    accept(0)
+//    accept(2) // Fantastic! Does not compile
+    val maybeBinary = Try {
+      args(0).toInt
+    }
+//      .map { i => accept(i)} // does not compile as we cannot guarantee it's 1 or 0
   }
 
 }
